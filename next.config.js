198c2d452fff55a90eeffdbe4f929662eaeb33ca
Akash Dhiman <akashdhiman96252@gmail.com>
2025-02-12 11:05:52 +0530

    new files and folders

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
     basePath: process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASEPATH : "",
     reactStrictMode: false,
     swcMinify: true,
+    eslint: {
+        ignoreDuringBuilds: true, 
+    },
+
 };
 
 module.exports = nextConfig;
